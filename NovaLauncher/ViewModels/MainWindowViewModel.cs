822e45b4b7b621d15591827408644cb58385a26f
Lightyear4509 <ravalom20905@gmail.com>
2026-07-20 21:25:20 -0400

    Add initial MainWindow view model

---
+++ NovaLauncher/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
+using NovaLauncher.Models;
+using NovaLauncher.Services;
+
+namespace NovaLauncher.ViewModels;
+
+public partial class MainWindowViewModel : ObservableObject
+{
+    private readonly GameLibraryService _libraryService;
+
+    public ObservableCollection<Game> Games { get; }
+
+    [ObservableProperty]
+    private Game? selectedGame;
+
+    [ObservableProperty]
+    private string statusText = "Status: Ready.";
+
+    public MainWindowViewModel()
+    {
+        _libraryService = new GameLibraryService();
+
+        Games = new ObservableCollection<Game>(
+            _libraryService.LoadGames());
+
+        if (Games.Count > 0)
+        {
+            SelectedGame = Games[0];
+            StatusText = "Status: Saved library loaded.";
+        }
+    }
+}
